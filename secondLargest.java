72645302c62f7ec44d66e5cb02f4d80c3792c0bd
HashTag18 <128501294+HashTag18@users.noreply.github.com>
2024-02-16 12:05:32 +0530

    2nd Largest number in Array
    User defined Input with generalised code giving largest and 2nd largest number as Output.

---
+++ secondLargest.java
@@ -0,0 +1,32 @@
+package ClgLabProj;
+import java.util.Scanner;
+public class secondLargest {
+    public static void main (String[] args){
+        int [] a= new int[5];
+        int l=0;
+        int Ltwo=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println( "Enter the number of elements in the array:");
+        int length=sc.nextInt();
+        
+            for(int j=0;j<length;j++)
+            {
+                    System.out.print("Enter element "+(j+1)+": ");
+                    a[j]=sc.nextInt();
+            }
+            for (int i=0;i<a.length;i++)
+            {
+                if(a[i]>Ltwo){
+                    if (a[i]>l) {
+                        Ltwo=l;
+                        l=a[i];
+                    }else{
+                        Ltwo=a[i];
+                    }
+                    }
+            }
+                System.out.println("Largest Number : "+l);
+                System.out.println("2nd Largest Number : "+Ltwo);
+            sc.close();
+        }
+    }
