244f4fa92be9e6e276e8fe8354834a1e8e4184da
HashTag18 <128501294+HashTag18@users.noreply.github.com>
2024-03-12 10:52:36 +0530

    Updated Indentations

--- secondLargest.java
+++ secondLargest.java
@@ -1,32 +1,34 @@
 package ClgLabProj;
+
 import java.util.Scanner;
+
 public class secondLargest {
-    public static void main (String[] args){
-        int [] a= new int[5];
-        int l=0;
-        int Ltwo=0;
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        int l = 0;
+        int Ltwo = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println( "Enter the number of elements in the array:");
-        int length=sc.nextInt();
-        
-            for(int j=0;j<length;j++)
-            {
-                    System.out.print("Enter element "+(j+1)+": ");
-                    a[j]=sc.nextInt();
-            }
-            for (int i=0;i<a.length;i++)
-            {
-                if(a[i]>Ltwo){
-                    if (a[i]>l) {
-                        Ltwo=l;
-                        l=a[i];
-                    }else{
-                        Ltwo=a[i];
-                    }
-                    }
-            }
-                System.out.println("Largest Number : "+l);
-                System.out.println("2nd Largest Number : "+Ltwo);
-            sc.close();
+        System.out.println("Enter the number of elements in the array:");
+        int length = sc.nextInt();
+
+        for (int j = 0; j < length; j++) {
+            System.out.print("Enter element " + (j + 1) + ": ");
+            a[j] = sc.nextInt();
         }
+        int i = 0;
+        while (i < a.length) {
+            if (a[i] > Ltwo) {
+                if (a[i] > l) {
+                    Ltwo = l;
+                    l = a[i];
+                } else {
+                    Ltwo = a[i];
+                }
+            }
+            i++;
+        }
+        System.out.println("Largest Number : " + l);
+        System.out.println("2nd Largest Number : " + Ltwo);
+        sc.close();
     }
+}
